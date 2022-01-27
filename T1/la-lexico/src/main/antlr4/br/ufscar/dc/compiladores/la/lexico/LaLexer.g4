90c6872bf6199c4d1a3d377118bc442c8677febd
Vitor Donadelli <vitor_donadelli@hotmail.com>
2022-01-26 22:05:04 -0300

    Adicionando documentação interna

--- T1/la-lexico/src/main/antlr4/br/ufscar/dc/compiladores/la/lexico/LaLexer.g4
+++ T1/la-lexico/src/main/antlr4/br/ufscar/dc/compiladores/la/lexico/LaLexer.g4
@@ -1,5 +1,6 @@
 lexer grammar LaLexer;
 
+/* Generalização das palavras reservadas*/ 
 TOKEN
         : PALAVRA_CHAVE
         | DECLARACAO
@@ -9,6 +10,7 @@ TOKEN
         | OPERADOR_ARITMETICO
         | CARACTERES;
 
+/* Palavras-chaves gerais*/ 
 fragment
 PALAVRA_CHAVE
 	: 'algoritmo'
@@ -37,6 +39,7 @@ PALAVRA_CHAVE
 	| 'procedimento'
 	| 'fim_procedimento';
 
+/* Declarações e tipos de variaveis*/ 
 fragment
 DECLARACAO
 	: 'declare'
@@ -47,17 +50,20 @@ DECLARACAO
 	| 'inteiro'
 	| 'real';
 
+/* Operadores logicos*/ 
 fragment
 OPERADOR_LOGICO
 	: 'e'
 	| 'ou'
 	| 'nao';
 
+/* Valores booleanos */ 
 fragment
 VALOR_BOOLEANO
         : 'verdadeiro'
 	| 'falso';
 
+/* Operadores relacionais*/ 
 fragment
 OPERADOR_COMPARACAO
 	: '='
@@ -68,6 +74,7 @@ OPERADOR_COMPARACAO
 	| '>='
 	| '<-';
 
+/* Operadores aritméticos*/ 
 fragment
 OPERADOR_ARITMETICO
 	: '+'
@@ -78,6 +85,7 @@ OPERADOR_ARITMETICO
 	| '^'
 	| '&';
 
+/* Caracteres especiais*/ 
 fragment
 CARACTERES
 	: '.'
@@ -89,18 +97,21 @@ CARACTERES
 	| '['
 	| ']';
 
+/* Identificadores, numeros reais e numeros inteiros*/ 
 IDENT: ('a'..'z'|'A'..'Z'|'_')('a'..'z'|'A'..'Z'|'0'..'9'|'_')*;
-
 NUM_INT: ('0'..'9')+;
-
 NUM_REAL: ('0'..'9')+ '.' ('0'..'9')+;
 
+/* Simbolos a serem ignorados*/ 
 WS: ( ' ' | '\t' | '\r' | '\n' );
 
+/* Comentários que leva em consideração somente texto dentro de um par de chaves { ... } */ 
 COMENTARIO:             '{' ( ~( '{' | '}' | '\n' ) )* '}';
 COMENTARIO_NAO_FECHADO: '{' ( ~( '{' | '}'        ) )* '\n';
 
+/* Cadeia de caracteres */ 
 CADEIA:             '"' ( '\\"' | ~( '"' | '\n' ) )* '"';
 CADEIA_NAO_FECHADA: '"' ( '\\"' | ~( '"'        ) )* '\n';
 
+/* Caso não seja identificado como alguma outra definição acima, cai aqui*/ 
 SIMBOLO_NAO_IDENTIFICADO: .;
--- T1/la-lexico/src/main/java/br/ufscar/dc/compiladores/la/lexico/Principal.java
+++ T1/la-lexico/src/main/java/br/ufscar/dc/compiladores/la/lexico/Principal.java
@@ -11,39 +11,56 @@ public class Principal {
     
     public static void main(String[] args) throws IOException {
         
+        // Inicia o FileWriter pra escrever no arquivo passado como entrada
         FileWriter fw = new FileWriter(new File(args[1]));
 
+        // Construção do lexer 
         CharStream cs = CharStreams.fromFileName(args[0]);
         LaLexer lex = new LaLexer(cs);
         
+        // Criação do Token
         Token t = null;
                 
         while ((t = lex.nextToken()).getType() != Token.EOF) {
             String s = LaLexer.VOCABULARY.getSymbolicName(t.getType());
-            // Exemplo de Retorno = <'algoritmo','algoritmo'>
-            //if (s.equals("PALAVRA_CHAVE") || s.equals("DECLARACAO") || s.equals("OPERADOR_LOGICO") || s.equals("VALOR_BOOLEANO") || s.equals("OPERADOR_COMPARACAO") || s.equals("OPERADOR_ARITMETICO") || s.equals("CARACTERES")) {
+                    
+            // Caso seja um TOKEN genérico criado na gramatica, ou seja
+            //if (s.equals("PALAVRA_CHAVE") || s.equals("DECLARACAO") || s.equals("OPERADOR_LOGICO") ||
+            //    s.equals("VALOR_BOOLEANO") || s.equals("OPERADOR_COMPARACAO") || s.equals("OPERADOR_ARITMETICO") || s.equals("CARACTERES"))
             if (s.equals("TOKEN")) {
+                // Retornaremos no seguinte exemplo: <'algoritmo','algoritmo'>
                 fw.write("<'" + t.getText() + "','" + t.getText() + "'>\n");
             } 
-            // Exemplo de Retorno = <'idade',IDENT>
+            
+            // Caso seja um numero inteiro, numero real, cadeia ou um identificador
             else if (s.equals("NUM_INT") || s.equals("NUM_REAL") || s.equals("CADEIA") || s.equals("IDENT")) {
+                 // Retornaremos no seguinte exemplo: <'nome',IDENT>
                 fw.write("<'" + t.getText() + "'," + LaLexer.VOCABULARY.getSymbolicName(t.getType()) + ">\n");
             } 
   
             // Verificação de Erros
+            // Comentário não fechado na mesma linha
             else if (s.equals("COMENTARIO_NAO_FECHADO")) {
+                // Retorno: Linha X: comentario nao fechado
                 fw.write("Linha " + t.getLine() + ": comentario nao fechado\n");
                 break;    
-            }                
+            }       
+            
+            // Cadeia não fechada na mesma linha
             else if (s.equals("CADEIA_NAO_FECHADA")) {
+                // Retorno: Linha X: cadeia literal nao fechada
                 fw.write("Linha " + t.getLine() + ": cadeia literal nao fechada\n");
                 break;
             } 
+            
+            // Simbolo que não encaixa nas definições da gramática
             else if (s.equals("SIMBOLO_NAO_IDENTIFICADO")) {
+                // Retorno: Linha X: SIMBOLO - simbolo nao identificado
                 fw.write("Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado\n");
                 break;
             }
-        }                   
+        }
+        // Fecha o arquivo de escrita
         fw.close();
     }
 }
